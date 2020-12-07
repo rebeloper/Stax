53becd70e8af01378c7e7dd01ce240df3521d3e1
Alex Nagy <therebeloper@gmail.com>
2020-12-07 07:37:57 +0200

    assets

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Stax",
-            dependencies: [],
-            exclude: ["ReadMeAssets"]
+            dependencies: []
             ),
         .testTarget(
             name: "StaxTests",
